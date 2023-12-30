a2db81469cd0d9cd3c8d3d5de681648f2e99d9bc
Andrew Finsand <andfinsand@gmail.com>
2023-12-30 00:56:39 -0800

    UploadButton and Main component refactored.

--- client/ui/src/components/Main.tsx
+++ client/ui/src/components/Main.tsx
@@ -3,14 +3,30 @@ import UploadButton from './buttons/UploadButton'
 const Main: React.FC = () => {
     return (
         <>
-            {/* Upload area */}
-            <div className='flex flex-col justify-center self-center rounded-lg bg-uploadAreaColor w-600 h-400 m-10 p-6' style={{ boxShadow: 'rgba(0, 0, 0, 0.15) 0px 10px 20px, rgba(0, 0, 0, 0.1) 0px 3px 6px' }}>
+            {/* Dropzone */}
+            <div className='flex flex-col justify-center self-center rounded-lg bg-dropZoneColor w-600 h-400 m-10 p-6' style={{ boxShadow: 'rgba(0, 0, 0, 0.15) 0px 10px 20px, rgba(0, 0, 0, 0.1) 0px 3px 6px' }}>
                 {/* Dotted border */}
                 <div className='flex justify-center border-2 border-dotted border-dottedColor rounded-md w-full h-full'>
-                    {/* Content wrapper */}
+
+                    {/* Content */}
                     <div className='flex self-center'>
-                        {/* Upload button */}
-                        <UploadButton />
+                        <div className='flex flex-col'>
+
+                            {/* Upload button */}
+                            <UploadButton />
+
+                            <div className='text-center text-grayFont text-xs mt-5'>or drop image here</div>
+
+                            {/* File parameters */}
+                            <div className='flex text-grayFont text-xs mt-8 gap-2.5'>
+                                <div className='bg-fileTypeContainer rounded-md py-1 px-2'>png</div>
+                                <div className='bg-fileTypeContainer rounded-md py-1 px-2'>jpeg</div>
+                                <div className='bg-fileTypeContainer rounded-md py-1 px-2'>webp</div>
+                                <div className='bg-fileTypeContainer rounded-md py-1 px-2'>svg</div>
+                                <div className='self-center'>up to 1000 x 1000px</div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
--- client/ui/src/components/buttons/UploadButton.tsx
+++ client/ui/src/components/buttons/UploadButton.tsx
@@ -11,6 +11,8 @@ const UploadButton: React.FC = () => {
 
     return (
         <>
+
+            {/* Hidden input field to receive image file */}
             <input
                 className='hidden'
                 ref={imageInputRef} //Saves a reference to the input element
@@ -19,26 +21,12 @@ const UploadButton: React.FC = () => {
                 accept='image/jpeg, image/png, image/webp, image/svg+xml'
             />
 
-            {/* Content */}
-            <div className='flex flex-col'>
+            {/* Choose image button */}
+            <button className='btn-hover-effect flex self-center drop-shadow-md rounded-lg py-4 px-7 hover:drop-shadow-none' onClick={openFileExplorer}>
+                <img src='/upload.svg' alt="Image upload icon" />
+                <div className='self-center text-sm pl-1'>Choose Image</div>
+            </button>
 
-                {/* Choose image button */}
-                <button className='btn-hover-effect flex self-center drop-shadow-md rounded-lg py-4 px-7 hover:drop-shadow-none' onClick={openFileExplorer}>
-                    <img src='/upload.svg' alt="Image upload icon" />
-                    <div className='self-center text-sm pl-1'>Choose Image</div>
-                </button>
-
-                <div className='text-center text-grayFont text-xs mt-5'>or drop image here</div>
-
-                {/* File parameters */}
-                <div className='flex text-grayFont text-xs mt-8 gap-2.5'>
-                    <div className='bg-fileTypeContainer rounded-md py-1 px-2'>png</div>
-                    <div className='bg-fileTypeContainer rounded-md py-1 px-2'>jpeg</div>
-                    <div className='bg-fileTypeContainer rounded-md py-1 px-2'>webp</div>
-                    <div className='bg-fileTypeContainer rounded-md py-1 px-2'>svg</div>
-                    <div className='self-center'>up to 1000 x 1000px</div>
-                </div>
-            </div>
         </>
     );
 }
